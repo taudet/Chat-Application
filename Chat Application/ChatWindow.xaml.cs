83a6f2f7246c44e8303c0eb9c9abf351ff0197ab
Thomas audet <thomas.audet@live.com>
2024-08-23 22:20:42 -0400

    The code now Connects to the server on button click and sends the message entered in the textbox

--- Chat Application/ChatWindow.xaml.cs
+++ Chat Application/ChatWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,31 +14,81 @@ using System.Windows.Shapes;
 
 namespace Chat_Application
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class ChatWindow : Window
     {
+        private TcpClient chatClient;
+        private NetworkStream chatStream;
+        private string clientID;
+        private string IpAddress;
+        private int Port;
+
+
         public ChatWindow()
         {
             InitializeComponent();
+
+            this.clientID = "1221121212";
+            this.IpAddress = "192.168.1.200";
+            this.Port = 2706;
         }
 
         public void sendButton_Click(object sender, RoutedEventArgs e)
         {
-            string OutgoingMessage = messageSendBox.Text.Trim();
+            try
+            {
+                chatClient = new TcpClient(IpAddress, Port);
+                chatStream = chatClient.GetStream();
 
-            messageReceiveBox.Text = OutgoingMessage;
+                string OutgoingMessage = messageSendBox.Text.Trim();
+
+                sendResponse(chatStream, clientID, OutgoingMessage);
+
+                messageSendBox.Clear();
+                
+            }
+            catch (Exception ex) 
+            {
+
+            }
         }
 
-        private void messageSendBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
 
+        public static void WriteStringToStream(NetworkStream stream, string data)
+        {
+            byte[] bytesToSend = Encoding.ASCII.GetBytes(data);
+            stream.Write(bytesToSend, 0, bytesToSend.Length);
         }
 
-        private void messageSendBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
 
+
+        public static void sendResponse(NetworkStream stream, string clientID, string chatMessage)
+        {
+            string stringToSend = clientID + ";" + chatMessage;
+            WriteStringToStream(stream, stringToSend);
+        }
+
+        private (TcpClient, string) ConnectClient(string server, int port)
+        {
+            TcpClient client = new TcpClient();
+            try
+            {
+                client.Connect(server, port);
+                NetworkStream stream = client.GetStream();
+
+                string clientID = Guid.NewGuid().ToString();
+
+                byte[] bytesToSend = Encoding.ASCII.GetBytes(clientID);
+                stream.Write(bytesToSend, 0, bytesToSend.Length);
+
+
+                return (client, clientID);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection failed");
+                client.Close();
+                return (null, null);
+            }
         }
     }
 
